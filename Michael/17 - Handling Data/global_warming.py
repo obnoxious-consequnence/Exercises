e1bd35d16d00dcb81333bafacafb0a59dee785e8
Gonron <lundsgaard1997@hotmail.dk>
2019-03-12 13:07:08 +0100

    added new exercise

--- Michael/17 - Handling Data/global_warming.py
+++ Michael/17 - Handling Data/global_warming.py
@@ -1,4 +1,8 @@
 def global_warming_status():
     return "The globe is heating up!"
 status = global_warming_status()
-print(status)
+
+if __name__ == "__main__":
+    status = global_warming_status()
+    print(status)
+    print(__name__)
