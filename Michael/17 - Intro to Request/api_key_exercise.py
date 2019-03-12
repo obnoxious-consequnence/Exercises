e4e8abc5167f9add103f8f748615bfc85d8840c7
Gonron <lundsgaard1997@hotmail.dk>
2019-03-12 15:06:34 +0100

    -

--- Michael/17 - Intro to Request/api_key_exercise.py
+++ Michael/17 - Intro to Request/api_key_exercise.py
@@ -1,5 +1,9 @@
 # Remember to make your own file with api_keys - DO NOT PUBLISH YOUR API KEYS
 import api_keys
+import requests
 
 url = "https://github.com/datsoftlyngby/dat4sem2019spring-python-materials"
 headers = {"Authorization": "token {}".format(api_keys.GITHUB_API_KEY)}
+
+r = requests.get(url, headers=headers)
+print(r.json)
