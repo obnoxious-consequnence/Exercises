f4c7cb716d98ab42f520b4bfbfe7a7c35b684687
Gonron <lundsgaard1997@hotmail.dk>
2019-03-19 13:34:24 +0100

    20 - cli exercise done

--- Michael/Sandbox/make_random_user.py
+++ Michael/Sandbox/make_random_user.py
@@ -3,18 +3,38 @@ import string
 from names import first_name, last_name
 from jobs import job
 
-name = first_name[random.randint(0,len(first_name))] + ' ' + last_name[random.randint(0,len(last_name))]
+# name = first_name[random.randint(0, len(first_name))] + ' ' + last_name[random.randint(0, len(last_name))]
 
-username_extra = ''.join(random.choice(string.digits) for i in range(4))
-username = ''.join(random.choice(name) for i in range(6)) + username_extra
+# username_extra = ''.join(random.choice(string.digits) for i in range(4))
+# username = ''.join(random.choice(name) for i in range(6)) + username_extra
+# username.replace(" ", "")
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-password = ''.join(random.choice(chars) for i in range(8))
+# chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+# password = ''.join(random.choice(chars) for i in range(8))
 
-email_extra = ''.join(random.choice(string.digits))
-email = name[:4].lower() + email_extra + '@yahoo.com'
+# email_extra = ''.join(random.choice(string.digits))
+# email = name[:4].lower() + email_extra + '@yahoo.com'
 
-job = job[random.randint(0,len(job))]
+# job = job[random.randint(0, len(job))]
 
-print('Name:', name, 'Username:', username, 'Password:',password, 'E-mail:',email, 'Job:',job)
+# print('Name:', name, '| Username:', username, '| Password:',password, '| E-mail:',email, '| Job:',job)
 
+def make_users():
+  name = first_name[random.randint(0,len(first_name))] + ' ' + last_name[random.randint(0,len(last_name))]
+
+  username_extra = ''.join(random.choice(string.digits) for i in range(4))
+  username = ''.join(random.choice(name) for i in range(6)) + username_extra
+  username.replace(" ", "")
+
+  chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+  password = ''.join(random.choice(chars) for i in range(8))
+
+  email_extra = ''.join(random.choice(string.digits))
+  email = name[:4].lower() + email_extra + '@yahoo.com'
+
+  jobb = job[random.randint(0,len(job))]
+
+  return ('Name:', name, '| Username:', username, '| Password:', password, '| E-mail:', email, '| Job:', jobb)
+
+for i in range(10):
+  print(make_users())
