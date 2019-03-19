bca39c67df61eb80433f9f32ac340d00ddc5ed12
Gonron <lundsgaard1997@hotmail.dk>
2019-03-19 08:26:27 +0100

    improved make_random_user in sandbox

--- Michael/Sandbox/make_random_user.py
+++ Michael/Sandbox/make_random_user.py
@@ -5,6 +5,9 @@ from jobs import job
 
 name = first_name[random.randint(0,len(first_name))] + ' ' + last_name[random.randint(0,len(last_name))]
 
+username_extra = ''.join(random.choice(string.digits) for i in range(4))
+username = ''.join(random.choice(name) for i in range(6)) + username_extra
+
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 password = ''.join(random.choice(chars) for i in range(8))
 
@@ -13,5 +16,5 @@ email = name[:4].lower() + email_extra + '@yahoo.com'
 
 job = job[random.randint(0,len(job))]
 
-print('Name:', name, 'Password:',password, 'E-mail:',email, 'Job:',job)
+print('Name:', name, 'Username:', username, 'Password:',password, 'E-mail:',email, 'Job:',job)
 
